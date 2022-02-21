d6e3f38f6dc452a5cbf054493f21252f1a0a9cf0
sbs0516 <tumtum@naver.com>
2022-02-21 19:25:31 +0900

    SplashActivity 작성

---
+++ app/src/main/java/com/example/dontcallindrunk/splash/SplashActivity.kt
@@ -0,0 +1,35 @@
+package com.example.dontcallindrunk.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.*
+import com.example.dontcallindrunk.MainActivity
+import com.example.dontcallindrunk.R
+import com.example.dontcallindrunk.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+
+    lateinit var splashViewDataBinding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        splashViewDataBinding = DataBindingUtil.setContentView(this, R.layout.activity_splash)
+
+        val splashViewModel = ViewModelProviders.of(this).get(SplashViewModel::class.java)
+        splashViewDataBinding.lifecycleOwner = this
+
+        splashViewModel.delay().observe(this, Observer {
+            if(it) {
+                goMainActivity()
+            }
+        })
+
+    }
+    private fun goMainActivity() {
+        val myIntent = Intent(this, MainActivity::class.java)
+        startActivity(myIntent)
+        finish()
+    }
+}
---
+++ app/src/main/java/com/example/dontcallindrunk/splash/SplashViewModel.kt
@@ -0,0 +1,20 @@
+package com.example.dontcallindrunk.splash
+
+import android.os.Handler
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SplashViewModel: ViewModel() {
+
+    var check = MutableLiveData<Boolean>()
+
+    fun delay(): LiveData<Boolean> {
+        val handler = Handler()
+        handler.postDelayed( {
+            check.value = true
+        }, 3000)
+        return check
+    }
+
+}
